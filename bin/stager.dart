99073301c0bdbb7f6d0ca5a8855af51a6e62eb61
Bryan Oltman <bryanoltman@gmail.com>
2022-07-22 14:01:24 -0400

    initial commit

---
+++ bin/stager.dart
@@ -0,0 +1,5 @@
+import 'package:stager/stager.dart' as stager;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stager.calculate()}!');
+}
---
+++ test/stager_test.dart
@@ -0,0 +1,8 @@
+import 'package:stager/stager.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
