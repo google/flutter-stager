86ca3b2ba1e286d2f67e10c6ffe2e0dd0653f29e
Bryan Oltman <bryanoltman@gmail.com>
2022-07-22 18:18:32 -0400

    Add generated app

---
+++ example/lib/posts_list/posts_list_scenes.stager_app.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StagerAppGenerator
+// **************************************************************************
+
+import 'posts_list_scenes.dart';
+import 'package:flutter/material.dart';
+import 'package:stager/stager.dart';
+
+Future<void> main() async {
+  final scenes = [EmptyListScene(), WithPostsScene(), LoadingScene()];
+
+  for (final scene in scenes) {
+    await scene.setUp();
+  }
+
+  runApp(SceneListApp(scenes: scenes));
+}
