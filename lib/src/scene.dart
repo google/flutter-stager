fc613081fe52edef55ef94c293db4a678236b98d
Bryan Oltman <bryanoltman@gmail.com>
2022-07-26 11:41:04 -0400

    Add doc comments to Scene

--- lib/src/scene.dart
+++ lib/src/scene.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/widgets.dart';
 
 abstract class Scene {
+  /// This Scene's name in the [StagerApp]'s list of scenes.
   String get title;
+
+  /// Used to configure this Scene's dependencies.
+  ///
+  /// Analogous to StatefulWidget's `initState`, this is called once at app
+  /// launch.
   Future<void> setUp() async {}
+
+  /// Creates the widget tree for this Scene.
+  ///
+  /// This is called on every rebuild, including by Hot Reload.
   Widget build();
 }
