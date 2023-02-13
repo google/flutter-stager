84c2121430394d1e04adee3ea7e82aeb19fda2a0
Bryan Oltman <bryanoltman@google.com>
2023-02-13 13:40:53 -0500

    Export NumberStepperControl in stager.dart

--- lib/stager.dart
+++ lib/stager.dart
@@ -1,5 +1,6 @@
 export 'src/control_panel/boolean_control.dart';
 export 'src/control_panel/dropdown_control.dart';
+export 'src/control_panel/number_stepper_control.dart';
 export 'src/environment_aware_app.dart';
 export 'src/scene.dart';
 export 'src/stager_app.dart';
