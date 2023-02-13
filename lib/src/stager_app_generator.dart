4e7ce950a2f63bd2a468b3c731c1f22f7c3f0b72
Bryan Oltman <bryanoltman@google.com>
2023-02-13 14:06:46 -0500

    Use `element` instead of `element2` in StagerAppGenerator

--- lib/src/stager_app_generator.dart
+++ lib/src/stager_app_generator.dart
@@ -17,7 +17,7 @@ class StagerAppGenerator extends Generator {
         .whereType<ClassElement>()
         .where(
           (ClassElement classElement) => classElement.allSupertypes.any(
-            (InterfaceType supertype) => supertype.element2.name == 'Scene',
+            (InterfaceType supertype) => supertype.element.name == 'Scene',
           ),
         )
         .where(
